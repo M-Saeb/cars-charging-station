c2602c9cb2e79623a25b5ca9805cbd7d6564668d
allerter <a.hazhir@gmail.com>
2023-11-19 19:10:33 +0100

    Update ChargingSlot.java

--- src/chargeStation/ChargingSlot.java
+++ src/chargeStation/ChargingSlot.java
@@ -1,32 +1,38 @@
 package chargeStation;
 
 import java.time.LocalDateTime;
-import java.lang.IllegalArgumentException;
+import java.util.logging.Logger;
+
 
 public class ChargingSlot {
 	private int id;
 	private ChargingStation chargingStation;
 	private Car currentCar = null;
 	private LocalDateTime nextFreeTime;
+	private float throughput;
+	private Logger logger;
 	
-	public ChargingSlot(chargingStation chargingStation, int id) {
+	public ChargingSlot(chargingStation chargingStation, int id, float throughput) {
 		if (chargingStation == null) {
 			throw new IllegalArgumentException("Supplied charging station is null.");
 		}
 		this.chargingStation = ChargingStation;
 		this.id = id;
+		this.logger = Logger.getLogger(this.getClass().getSimpleName() + " " + this.id);
+		this.logger.fine("Initiated Charging Slot " + this.id);
 	}
 	
 	public void chargeCar(Car car) throws  ChargingSlotFullException{
 		// if there is a car already docked in this slot, raise an exception
 		if (this.currentCar != null) {
-			throw new ChargingSlotFullException("Slot +" this.id + " is full.");
+			throw new ChargingSlotFullException("Slot " + this.id + " is full.");
 		}
 		this.currentCar = car;
 		
 		// calculate when the charging finishes and the slot is available
-		// LocalDateTime finishedChargingTime = ;
-		// this.nextFreeTime = finishedChargingTime;
+		this.nextFreeTime = this.calculateNextFreeTime();
+		this.logger.info("Plugged in " + this.currentCar.toString());
+		this.logger.fine("Slot will be free at " + this.nextFreeTime.toString());
 	}
 	
 	public int getId(){
@@ -40,7 +46,14 @@ public class ChargingSlot {
 	public LocalDateTime getNextFreeTime() {
 		return this.nextFreeTime;
 	}
+	
+	private LocalDateTime calculateNextFreeTime() {
+		long chargingTime =  (long) this.currentCar.getChargingTime(this.chargingStation);
+		return LocalDateTime.now().plusSeconds(chargingTime);
 	}
 	
-	
+	public String toString() {
+		return String.format("Charging Slot %s", this.id);
+	} 
 }
+	
