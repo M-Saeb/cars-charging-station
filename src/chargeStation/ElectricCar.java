2884e626b2f81d2fa2c3de2a99df65afdab7532b
allerter <a.hazhir@gmail.com>
2023-11-20 13:58:26 +0100

    Update Gas and Electric Car constructor

--- src/chargeStation/ElectricCar.java
+++ src/chargeStation/ElectricCar.java
@@ -3,9 +3,9 @@ package chargeStation;
 public class ElectricCar extends Car
 {
 
-	public ElectricCar(String carNumber, float tankCapacity, float waitDuration, LocationAPI api)
+	public ElectricCar(String carNumber, float tankCapacity, float waitDuration, LocationAPI api, float[][] currentGPS)
 	{
-		super(carNumber, tankCapacity, waitDuration, api);
+		super(carNumber, tankCapacity, waitDuration, api, currentGPS);
 		// TODO Auto-generated constructor stub
 	}
 	
--- src/chargeStation/GasCar.java
+++ src/chargeStation/GasCar.java
@@ -3,9 +3,9 @@ package chargeStation;
 public class GasCar extends Car
 {
 
-	public GasCar(String carNumber, float tankCapacity, float waitDuration, LocationAPI api)
+	public GasCar(String carNumber, float tankCapacity, float waitDuration, LocationAPI api, float[][] currentGPS)
 	{
-		super(carNumber, tankCapacity, waitDuration, api);
+		super(carNumber, tankCapacity, waitDuration, api, currentGPS);
 		// TODO Auto-generated constructor stub
 	}
 	
