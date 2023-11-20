44559c87685977b53abec8b9a406de529d8621d6
allerter <a.hazhir@gmail.com>
2023-11-20 10:25:14 +0100

    Rename chargeStation attributes to sane names

--- src/chargeStation/Stations/ChargingStation.java
+++ src/chargeStation/Stations/ChargingStation.java
@@ -6,46 +6,46 @@ public class ChargingStation
     GPS location from every charging station.
     This value can be sent to the other types of station from this super class
      */
-    private float GPSLatitud_f;
-    private float GPSLongitud_f;
+    private float GPSLatitude;
+    private float GPSLongitude;
     /* TODO: Implement queue logic, where variable availableSlots_int is the number of slot available to push into the queue */
     /*
     Amount of available slot per charging station
      */
-    private int availableSlots_int;
-    private int chargingStationID_int;
+    private int availableSlots;
+    private int chargingStationID;
     private float outputPerSecond;
 
     public float getGPSLatitud_f() {
-        return GPSLatitud_f;
+        return GPSLatitude;
     }
 
     public void setGPSLatitud_f(float GPSLatitud_f) {
-        this.GPSLatitud_f = GPSLatitud_f;
+        this.GPSLatitude = GPSLatitud_f;
     }
 
     public float getGPSLongitud_f() {
-        return GPSLongitud_f;
+        return GPSLongitude;
     }
 
     public void setGPSLongitud_f(float GPSLongitud_f) {
-        this.GPSLongitud_f = GPSLongitud_f;
+        this.GPSLongitude = GPSLongitud_f;
     }
 
     public int getAvailableSlots_int() {
-        return availableSlots_int;
+        return availableSlots;
     }
 
     public void setAvailableSlots_int(int availableSlots_int) {
-        this.availableSlots_int = availableSlots_int;
+        this.availableSlots = availableSlots_int;
     }
 
     public void setChargingStationID_int(int chargingStationID_int) {
-        this.chargingStationID_int = chargingStationID_int;
+        this.chargingStationID = chargingStationID_int;
     }
 
     public int getChargingStationID_int() {
-        return chargingStationID_int;
+        return chargingStationID;
     }
 
 	public float getOutputPerSecond()
