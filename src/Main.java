6312116d341fa2976e97a0bc98e1e3218c29ec8b
lhckmn <leander.hackmann@gmail.com>
2023-11-23 19:16:02 +0100

    Implemented  getNearestFreeChargingStation(). Changed types of example cars.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import api.GPSValues;
 import api.LocationAPI;
 import car.*;
+import exceptions.ChargingStationNotFoundException;
 import exceptions.InvalidGPSLatitudeException;
 import exceptions.InvalidGPSLongitudeException;
 import exceptions.InvalidGPSValueException;
-import stations.ChargingStation;
+import stations.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ChargingStation[] stations = new ChargingStation[4];
-		ChargingStation[] sortedStations = new ChargingStation[4];
-
+		
 		// create pool of charging stations
 		try {
-			stations[0] = new ChargingStation(1, new GPSValues(44, 96), 3, 10);
-			stations[1] = new ChargingStation(2, new GPSValues(10, 50), 2, 5);
-			stations[2] = new ChargingStation(3, new GPSValues(-50, 150), 4, 15);
-			stations[3] = new ChargingStation(4, new GPSValues(-70, 44), 1, 5);
+			stations[0] = new GasStation(1, new GPSValues(44, 96), 3, 10);
+			stations[1] = new GasStation(2, new GPSValues(10, 50), 2, 5);
+			stations[2] = new ElectricStation(3, new GPSValues(-50, 150), 4, 15);
+			stations[3] = new ElectricStation(4, new GPSValues(-70, 44), 1, 5);
 		} catch (InvalidGPSLatitudeException | InvalidGPSLongitudeException | InvalidGPSValueException e) {
 			System.out.println(e.toString());
 			return;
@@ -25,19 +25,16 @@ public class Main {
 		
 		// create pool of cars
 		Car[] cars = {
-				new GasCar("Ford Mustang", (float) 60.9, (float) 120.0, new LocationAPI(stations),
-						new GPSValues(40, 20)),
-				new ElectricCar("Toyota Prius", (float) 13.0, (float) 30.0, new LocationAPI(stations),
-						new GPSValues(-50, 50)),
-				new GasCar("Peugeot 206", (float) 49.0, (float) 30.0, new LocationAPI(stations),
-						new GPSValues(24, 140)),
+				new GasCar("Ford Mustang", (float) 60.9, (float) 120.0, new LocationAPI(stations), new GPSValues(40, 20)),
+				new ElectricCar("Toyota Prius", (float) 13.0, (float) 30.0, new LocationAPI(stations), new GPSValues(-50, 50)),
+				new GasCar("Peugeot 206", (float) 49.0, (float) 30.0, new LocationAPI(stations), new GPSValues(24, 140)),
 				new ElectricCar("BW i5", (float) 83.9, (float) 70.0, new LocationAPI(stations), new GPSValues(80, 95)),
 				new GasCar("Audi A3", (float) 48.0, (float) 90.0, new LocationAPI(stations), new GPSValues(40, 10))
 		};
 		System.out.println("Created cars and charging stations.");
-
+		
 		// create pool of threads
-
+		
 		// send cars to charging stations
 
 	}
